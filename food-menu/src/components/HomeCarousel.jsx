81108ff267470d584d6d016e0757877cb720aa3f
SENG Seyha <sengseyha2001@gmail.com>
2024-03-01 19:07:56 +0700

    add images banner

--- food-menu/src/components/HomeCarousel.jsx
+++ food-menu/src/components/HomeCarousel.jsx
@@ -6,11 +6,9 @@ export default function HomeCarousel() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel leftControl=" " rightControl=" " pauseOnHover>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src="https://i.ytimg.com/vi/LyvM9bXe8zI/maxresdefault.jpg" alt="..." />
+        <img src="https://img.freepik.com/free-psd/delicious-burger-food-menu-facebook-cover-template_106176-744.jpg" alt="..." />
+        <img src="https://as2.ftcdn.net/v2/jpg/04/87/18/99/1000_F_487189984_gMi84oHVh08azpPw7FpA5nFS7puxlgoj.jpg" alt="..." />
       </Carousel>
     </div>
 
